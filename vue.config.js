69aacf4d2d7c37027eb3841cf747c5a63227fe63
szw <2574387495@qq.com>
2023-09-08 18:21:11 +0800

    尚品汇-Header&Footer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  // 关闭eslint
-  lintOnSave: false,
-
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false
 })
